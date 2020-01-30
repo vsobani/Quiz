7c95b8cddcba48ba5fc85583effb7c4d46575970
vsobani <vishal.sobani@gmail.com>
2020-01-30 16:39:02 +0530

    Quiz application

---
+++ quiz/src/index.js
@@ -0,0 +1,69 @@
+import React,{Component} from "react";
+import "./assets/style.css";
+import ReactDOM from "react-dom";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
+
+class QuiZApp extends React.Component{
+    state = {
+        questionBank : [],
+        score:0,
+        response : 0
+
+    };
+
+
+ 
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank : question
+            });
+        });
+    }
+    componentDidMount(){
+        this.getQuestions();
+    }
+
+    correctAnswer = (answer,correctAnswer) => {
+        if (answer === correctAnswer){
+            this.setState({
+                score : this.state.score + 1
+            })
+        }
+        this.setState({
+            response : this.state.response < 5 ? this.state.response + 1 : 5
+        })
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score:0,
+            response:0
+        })
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="title">Quiz Application</div> 
+                {this.state.questionBank.length > 0 && 
+                this.state.response < 5 &&
+                this.state.questionBank.map(
+                 ({question,answers,correct,questionId}) => 
+                 <QuestionBox 
+                 question={question} 
+                 options={answers} 
+                 key={questionId}
+                 selected={answer => this.correctAnswer(answer , correct)}
+                 
+                 />
+                )}
+                {this.state.response === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>): null}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<QuiZApp />,document.getElementById("root"));
